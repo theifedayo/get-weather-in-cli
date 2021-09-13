a5a3343c6a3f4edea725b8f9f2484a558f22744b
theifedayo <adesiyanifedayo@gmail.com>
2021-09-13 20:57:19 +0100

    Add getRandomLocationWeather and getRandomLocationWeatherFull

--- source/services/weather.js
+++ source/services/weather.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
+const chalk = require('chalk');
 const networkHandler = require('./network.js');
-
+const { getWeatherIcon } = require('../helpers.js');
+const random = require('random-names-places');
 
 
 
@@ -19,8 +21,10 @@ getLocationWeather = async (param) => {
 	try{
 		let networkhandler = new NetworkHandler();
 		var weatherData = await networkhandler.getData(`${openWeatherMapURL}?q=${param}&units=metric&appid=${process.env.API_KEY}`);
-		console.log(weatherData);
+		let data = weatherData.weather
+		console.log(`${data[0].description} at ${param}. It is currently ${weatherData.main.temp}° ${data[0].icon} ${getWeatherIcon(data[0].id)}`);
 	}catch(err){
+		console.log(err)
 		console.log('An error occurred! ⚠️ ');
 	}
 }
@@ -36,13 +40,11 @@ getLocationWeatherFull = async (param) => {
 		let networkhandler = new NetworkHandler();
 		var weatherData = await networkhandler.getData(`${openWeatherMapURL}?q=${param}&units=metric&appid=${process.env.API_KEY}`);
 		if(weatherData != undefined){
-			console.log(`Weather data for ${param}`);
+			console.log(chalk.green(`Weather data for ${param}`));
         	console.log(weatherData);	
 		}else{
 			console.log('weather data is undefined 😔');
-		}
-		
-		
+		}			
 	}catch(err){
 		console.log('An error occurred! ⚠️');
 	}
@@ -53,30 +55,44 @@ getLocationWeatherFull = async (param) => {
 
 
 
-getRandomLocatiionWeather = async (param) => {
+getRandomLocationWeather = async () => {
 	try{
-
+		let param = random.place();
+		let networkhandler = new NetworkHandler();
+		var weatherData = await networkhandler.getData(`${openWeatherMapURL}?q=${param}&units=metric&appid=${process.env.API_KEY}`);
+		let data = weatherData.weather
+		console.log(`${data[0].description} at ${param}. It is currently ${weatherData.main.temp}° ${data[0].icon} ${getWeatherIcon(data[0].id)}`);
 	}catch(err){
-		console.log('An error occurred! ⚠️');
+		console.log('An error occurred! ⚠️ ');
 	}
 }
 
-getRandomLocatiionWeatherFull = async (param) => {
+getRandomLocationWeatherFull = async () => {
 	try{
-
+		let param = random.place();
+		let networkhandler = new NetworkHandler();
+		var weatherData = await networkhandler.getData(`${openWeatherMapURL}?q=${param}&units=metric&appid=${process.env.API_KEY}`);
+		if(weatherData != undefined){
+			console.log(chalk.green(`Weather data for ${param}`));
+        	console.log(weatherData);	
+		}else{
+			console.log('weather data is undefined 😔');
+		}			
 	}catch(err){
 		console.log('An error occurred! ⚠️');
 	}
 }
 
-
-getLocationWeatherFull('Jos');
+// getLocationWeather('Ile-Ife')
+// getLocationWeatherFull('Ile-Ife')
+// getRandomLocationWeather();
+// getRandomLocationWeatherFull();
 
 module.exports = {
     getLocationWeather,
     getLocationWeatherFull,
-    getRandomLocatiionWeather,
-    getRandomLocatiionWeatherFull
+    getRandomLocationWeather,
+    getRandomLocationWeatherFull
 };
 
 
